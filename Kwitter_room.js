096f789b83dc2b6d7b8ce34115f3850a9c859abe
sid5600000 <86678953+sid5600000@users.noreply.github.com>
2022-02-03 18:36:28 +0530

    Add files via upload

--- Kwitter_room.js
+++ Kwitter_room.js
@@ -15,3 +15,24 @@ function update_label(){
     username = localStorage.getItem("username");
     document.getElementById("user_name").innerHTML = "Welcome " + username + " !";
 }
+
+function add_room(){
+    room_name = document.getElementById("room_name").value;
+    localStorage.setItem("room_name",room_name);
+    firebase.database().ref("/").child(room_name).update({
+        purpose : "adding room"
+    });
+    window.location = "kwitter_page.html";
+}
+
+function getData(){firebase.database().ref("/").on('value',function(snapshot) {document.getElementById("output").innerHTML ="";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+    Room_names = childKey;
+    row = "<div id=" + Room_names + "class='room_name'" + "onclick='redirect_to_roomname(this.id)' >#" + Room_names + "</div>";
+    document.getElementById("output").innerHTML += row; 
+});});}
+getData();
+
+function redirect_to_roomname(room_name){
+    localStorage.setItem("room_name",room_name);
+    window.location = "kwitter_page.html";
+}
